2897c918010aef8a6141a72d777a2a0dcaa194a2
BaselSayeh <basel.sayeh@hotmail.com>
2018-06-15 01:52:07 +0300

    Initial commit

---
+++ make_package.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cd package
+
+rm .PKGINFO
+rm ../filediskroot.pkg.tar.xz
+
+touch ".PKGINFO"
+
+echo "pkgname = initcpio-filediskroot"                            >> ".PKGINFO"
+echo "pkgver = 1.0.0-1"                                           >> ".PKGINFO"
+echo "pkgdesc = initcpio filediskroot module"                     >> ".PKGINFO"
+echo "url = https://github.com/baselsayeh/filediskroot"           >> ."PKGINFO"
+echo "builddate = $(date +%s)"                                    >> ".PKGINFO"
+echo "packager = BaselSayeh <basel.sayeh@hotmail.com>"            >> ".PKGINFO"
+echo "size = $(du -shb usr | cut -f 1)"                           >> ".PKGINFO"
+echo "arch = x86_64"                                              >> ".PKGINFO"
+
+chown -R root:root * .*
+
+tar -cf - .PKGINFO * | xz -c -z - > ../filediskroot.pkg.tar.xz
+
+rm .PKGINFO
+
+cd ..
+
